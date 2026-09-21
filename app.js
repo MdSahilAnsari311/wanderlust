08d51e6c32f72461666947265ac0822f7676a641
Md Sahil Ansari <sahilansari66965@gmail.com>
2026-09-21 23:42:48 +0530

    Add Validation for schema using middleware

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js"); 
+const ExpressError = require("./utils/ExpressError.js"); 
+const cors = require("cors");
+const { listingSchema } = require("./schema.js");
+
+app.use(cors({
+    origin: "https://hoppscotch.io/"
+}));
 
 
 app.set("view engine", "ejs");
@@ -30,11 +37,26 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+});
+
+// Validation for schema (Using Middleware)
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else {
+        next();
+    }
+};
+
 // Index Route
-app.get("/listings",  async (req, res) => {
+app.get("/listings",  wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("./listings/index.ejs", {allListings});
-}); 
+})); 
 
 // New Route
 app.get("/listings/new", (req, res) => {
@@ -42,46 +64,50 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", {listing});
-});
+}));
 
 // Create Route
-app.post("/listings", wrapAsync(async (req, res) => {
-    let newListing = new Listing(req.body.listing);
+app.post("/listings", validateListing , wrapAsync(async (req, res) => {
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 }));
 
 // Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/edit.ejs", {listing}); 
-});
+}));
 
 // Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing ,wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing},  {runValidators : true});
     res.redirect("/listings");
-});
+}));
 
 // Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+
+// Handling Random Route Request
+app.use((req, res) => {
+    throw new ExpressError(404, "Page Not Found!");
 });
 
-app.get("/", (req, res) => {
-    res.send("Hi, I am root");
-});
-
+// Error Handling Middleware
 app.use((err, req, res, next) => {
-    res.send("Something want wrong!");
+    let {statusCode = 500, message = "Something went wrong!"} = err;
+    res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(port, () => {
